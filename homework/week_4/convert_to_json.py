0b149846a64c85344a47ecd0f73556e83e77ca43
kim66003 <kim66003@gmail.com>
2018-11-22 21:25:13 +0100

    wie weet werkt het nu wel

---
+++ homework/week_4/convert_to_json.py
@@ -0,0 +1,31 @@
+import csv
+import json
+import pandas as pd
+
+INPUT_FILE = "childvaccination.csv"
+OUTPUT_DF = "dataframe.csv"
+OUTPUT_FILE = "childvaccination.json"
+
+# create dataframe
+def create_dataframe(filename):
+    dataframe = pd.read_csv(filename, usecols=["LOCATION","SUBJECT","TIME"])
+    return dataframe
+
+def preprocess(dataframe):
+    dataframe = dataframe.dropna()
+    dataframe = dataframe.sort_values(['TIME'],ascending=True)
+    return dataframe
+
+# save dataframe to csv outfile
+def save_csv(filename, dataframe):
+    dataframe.to_csv(filename)
+
+# convert dataframe to json file
+def convert_json(filename, dataframe):
+    outfile = dataframe.to_json(filename, orient='index')
+
+if __name__ == '__main__':
+    df = create_dataframe(INPUT_FILE)
+    df = preprocess(df)
+    save_csv(OUTPUT_DF, df)
+    convert_json(OUTPUT_FILE, df)
