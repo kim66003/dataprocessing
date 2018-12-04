213efbfe9bf0d6603cf151022b7ac9df040fa9e1
kim66003 <kim66003@gmail.com>
2018-12-04 13:15:13 +0100

    hoi

--- homework/week_5/scatter.js
+++ homework/week_5/scatter.js
@@ -13,15 +13,21 @@ window.onload = function() {
       tob = response[1]
       obe = response[2]
       alcCountries = Object.keys(alc)
+      console.log(alcCountries)
       tobCountries = Object.keys(tob)
       obeCountries = Object.keys(obe)
       alcValues = valuesArray(alc, alcCountries)
+      minAlc = Math.min.apply(null, alcValues)
+      maxAlc = Math.max.apply(null, alcValues)
       tobValues = valuesArray(tob, tobCountries)
       obeValues = valuesArray(obe, obeCountries)
+      minObe = Math.min.apply(null, obeValues)
+      maxObe = Math.max.apply(null, obeValues)
       alcTob = makeCoordinates(tobValues, alcValues)
       alcObe = makeCoordinates(alcValues, obeValues)
       tobObe = makeCoordinates(tobValues, obeValues)
-      createMargin()
+      valuesScatter = createMarginScales(minAlc, maxAlc, minObe, maxObe, alcObe)
+      createScatter(valuesScatter[0], valuesScatter[1], valuesScatter[2], valuesScatter[3], alcObe, alcCountries)
   }).catch(function(e){
       throw(e);
   })
@@ -43,37 +49,107 @@ function makeCoordinates(array1, array2) {
           temp = [array1[i], array2[i]]
           output.push(temp);
       };
-      console.log(output)
       return output
 }
 
-function createMargin() {
-      var margin = {top: 20, right: 20, bottom: 20, left: 20},
-        padding = {top: 60, right: 60, bottom: 60, left: 60},
-        outerWidth = 1200,
-        outerHeight = 500,
-        innerWidth = outerWidth - margin.left - margin.right,
-        innerHeight = outerHeight - margin.top - margin.bottom,
-        width = innerWidth - padding.left - padding.right,
-        height = innerHeight - padding.top - padding.bottom;
+var margin = {top: 10, right: 20, bottom: 20, left: 10},
+  padding = {top: 10, right: 60, bottom: 60, left: 30},
+  outerWidth = 900,
+  outerHeight = 480,
+  innerWidth = outerWidth - margin.left - margin.right,
+  innerHeight = outerHeight - margin.top - margin.bottom,
+  width = innerWidth - padding.left - padding.right,
+  height = innerHeight - padding.top - padding.bottom;
 
-    //     var x = d3.scale.identity()
-    // .domain([0, width]);
-    //
-    //     var y = d3.scale.identity()
-    //         .domain([0, height]);
-    //
-    //     var xAxis = d3.svg.axis()
-    //         .scale(x)
-    //         .orient("bottom");
-    //
-    //     var yAxis = d3.svg.axis()
-    //         .scale(y)
-    //         .orient("right");
+function createMarginScales(minX, maxX, minY, maxY, dataset) {
+      var xScale = d3.scaleLinear()
+        .range([(margin.left + padding.left), width])
+        .domain([0, maxX])
+        .nice();
 
-        var svg = d3.select("body").append("svg")
-            .attr("width", outerWidth)
-            .attr("height", outerHeight)
-            .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      var yScale = d3.scaleLinear()
+        .range([height, (margin.top + padding.top)]).clamp(true)
+        .domain([0, maxY])
+        .nice();
+
+      var xAxis = d3.axisBottom(xScale);
+      var yAxis = d3.axisLeft(yScale);
+
+      var svg = d3.select("body").append("svg")
+          .attr("width", outerWidth)
+          .attr("height", outerHeight)
+          .append("g")
+          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+      var g = svg.append("g")
+          .attr("transform", "translate(" + padding.left + "," + padding.top + ")");
+
+      g.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(xAxis);
+
+      g.append("g")
+          .attr("class", "y axis")
+          .attr("transform", "translate(" + (margin.left + padding.left) + ",0)")
+          .call(yAxis);
+
+       return [xScale, yScale, svg, g]
+    }
+
+function createScatter(xScale, yScale, svg, g, dataset, labels) {
+      var gdots =  svg.selectAll("g.dot")
+            .data(dataset)
+            .enter().append('g');
+
+      svg.selectAll("circle")
+         .data(dataset)
+         .enter()
+         .append("circle")
+         .attr("cx", function(d) {
+            return xScale(d[0]);
+         })
+         .attr("cy", function(d) {
+            return yScale(d[1]) + margin.top;
+         })
+         .attr("r", function(d) {
+            return (d[1] / 8);
+         })
+         .attr("opacity", function(d) {
+           return (d[1] / 100)
+         })
+         .style("fill", "#5400ff");
+
+     gdots.append("text").text(function(d, i){
+                  return labels[i];
+               })
+               .attr("x", function (d) {
+                   return xScale(d[0]);
+               })
+               .attr("y", function (d) {
+                   return yScale(d[1]);
+               })
+               .attr("font-family", "sans-serif")
+               .attr("font-size", "10px")
+               .style("font-weight", "bold")
+               .attr("fill", "#ff009d");
+
+         svg.append("text")
+            .attr("class", "axis labels")
+            .attr("transform",
+                  "translate(" + ((width / 1.8)) + " ," +
+                                 (height + padding.bottom - 10) + ")")
+            .text("Alcoholconsumption")
+            .style("text-anchor", "middle")
+            .style("font-size", "16px");
+
+          svg.append("text")
+            .attr("class", "axis labels")
+            .attr("transform", "rotate(-90)")
+            .attr("x", 0 - (height / 1.7))
+            .attr("y", 0 + margin.left)
+            .attr("dy", "1em")
+            .text("Self-reported obesitas")
+            .style("text-anchor", "middle")
+            .style("font-size", "16px");
 }
--- homework/week_5/scatter.py
+++ homework/week_5/scatter.py
@@ -5,28 +5,30 @@ import pandas as pd
 INPUT_FILE = "dailysmokers.csv"
 INPUT_FILE2 = "alcoholconsumption.csv"
 INPUT_FILE3 = "obesitas.csv"
-OUTPUT_FILE = "dailysmokers2.json"
-OUTPUT_FILE2 = "alcoholconsumption2.json"
-OUTPUT_FILE3 = "obesitas2.json"
+OUTPUT_FILE = "dailysmokers.json"
+OUTPUT_FILE2 = "alcoholconsumption.json"
+OUTPUT_FILE3 = "obesitas.json"
 
 # create dataframe
 def create_dataframe(filename):
     dataframe = pd.read_csv(filename, usecols=["LOCATION", "INDICATOR", "SUBJECT", "TIME", "Value"])
     return dataframe
 
+countries = ["AUT", "CAN", "CZE", "FIN", "FRA", "GRC", "HUN", "ITA", "KOR", "LUX", "NLD", "POL", "PRT", "SVK", "ESP", "SWE", "TUR", "USA", "EST", "ISR", "SVN", "LVA"]
+
 def preprocess(dataframe):
     dataframe = dataframe.dropna()
     dataframe = dataframe[(dataframe['TIME']==2014)]
-    print(list(dataframe['LOCATION']))
-    dataframe = dataframe.set_index('LOCATION')
+    dataframe = dataframe.loc[dataframe['LOCATION'].isin(countries)]
+    dataframe = dataframe.reset_index(drop=True)
     return dataframe
 
 def preprocess2(dataframe):
     dataframe = dataframe.dropna()
     dataframe = dataframe[(dataframe['TIME']==2014)]
+    dataframe = dataframe.loc[dataframe['LOCATION'].isin(countries)]
     dataframe = dataframe[(dataframe['SUBJECT']=='SELFREPORTED')]
-    print(list(dataframe['LOCATION']))
-    dataframe = dataframe.set_index('LOCATION')
+    dataframe = dataframe.reset_index(drop=True)
     return dataframe
 
 # convert dataframe to json file
@@ -35,11 +37,14 @@ def convert_json(filename, dataframe):
 
 if __name__ == '__main__':
     df = create_dataframe(INPUT_FILE)
+    keys = set(df['LOCATION'])
     df = preprocess(df)
     convert_json(OUTPUT_FILE, df)
     df2 = create_dataframe(INPUT_FILE2)
+    keys2 = set(df2['LOCATION'])
     df2 = preprocess(df2)
     convert_json(OUTPUT_FILE2, df2)
     df3 = create_dataframe(INPUT_FILE3)
+    keys3 = set(df3['LOCATION'])
     df3 = preprocess2(df3)
     convert_json(OUTPUT_FILE3, df3)
