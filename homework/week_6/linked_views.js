dad673f6bf1a2ed845b6ba164f3788a2ceeb54fa
kim66003 <kim66003@gmail.com>
2018-12-10 21:05:43 +0100

    hoi

--- homework/week_6/linked_views.js
+++ homework/week_6/linked_views.js
@@ -21,11 +21,14 @@ Promise.all(requests).then(function(response) {
     coca_whole_us = response[6]
     hero_whole_us = response[7]
 
-    console.log(coca_ret_eu)
-
-    for (i = 0, len = coca_ret_eu.length, array = []; i < len; i++) {
-      console.log(coca_ret_eu[i])
-    }
+    creu = arrayValues(coca_ret_eu)[0]
+    console.log(creu)
+    creu_all = arrayValues(coca_ret_eu)[1]
+    minY = Math.min.apply(null, creu_all)
+    maxY = Math.max.apply(null, creu_all)
+    years = arrayValues(coca_ret_eu)[2]
+    minX = Math.min.apply(null, years)
+    maxX = Math.max.apply(null, years)
 
     // create margins and padding
     var margin = {top: 10, right: 20, bottom: 20, left: 10},
@@ -38,19 +41,20 @@ Promise.all(requests).then(function(response) {
       height = innerHeight - padding.top - padding.bottom;
 
       // create x and y scale
-      // var xScale = d3.scaleLinear()
-      //   .range([(margin.left + padding.left), width])
-      //   .domain([0, maxX])
-      //   .nice();
-      //
-      // var yScale = d3.scaleLinear()
-      //   .range([height, (margin.top + padding.top)]).clamp(true)
-      //   .domain([0, maxY])
-      //   .nice();
-      //
-      // // x and y axis to scale
-      // var xAxis = d3.axisBottom(xScale);
-      // var yAxis = d3.axisLeft(yScale);
+      var xScale = d3.scaleLinear()
+        .range([(margin.left + padding.left), width])
+        .domain([minX, maxX])
+        .nice();
+
+      var yScale = d3.scaleLinear()
+        .range([height, (margin.top + padding.top)]).clamp(true)
+        .domain([0, maxY])
+        .nice();
+
+      // x and y axis to scale
+      var xAxis = d3.axisBottom(xScale)
+                  .tickFormat(function(d){ return d });
+      var yAxis = d3.axisLeft(yScale);
 
       // create svg
       var svg = d3.select("body").append("svg")
@@ -59,8 +63,52 @@ Promise.all(requests).then(function(response) {
           .append("g")
           .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+      // create group tag
+      var g = svg.append("g")
+          .attr("transform", "translate(" + padding.left + "," + padding.top + ")");
+
+      // make x and y axis
+      g.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(xAxis);
+
+      g.append("g")
+          .attr("class", "y axis")
+          .attr("transform", "translate(" + (margin.left + padding.left) + ",0)")
+          .call(yAxis);
+
+      // d3's line generator
+      var line = d3.line()
+          .x(function(d) { return xScale(d); }) // set the x values for the line generator
+          .y(function(d) { return yScale(d); }) // set the y values for the line generator
+          .curve(d3.curveMonotoneX) // apply smoothing to the line
+
+      // Append the path, bind the data, and call the line generator
+      svg.append("path")
+          .datum(creu[0]) // Binds data to the line
+          .attr("class", "line") // Assign a class for styling
+          .attr("d", line); // Calls the line generator
+
 }).catch(function(e){
     throw(e);
 });
 
 };
+
+function arrayValues(data) {
+  values = []
+  values_all = []
+  for (key in data) {
+    years = []
+    temp_values = []
+    country = data[key]
+    for (key in country){
+        years.push(Number(key))
+        temp_values.push(country[key])
+        values_all.push(country[key])
+    }
+    values.push(temp_values)
+  }
+  return [values, values_all, years]
+}
