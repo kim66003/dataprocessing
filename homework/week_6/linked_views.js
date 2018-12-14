496f2ef7b7bab815094e391ffaeae752e68892db
kim66003 <kim66003@gmail.com>
2018-12-14 18:19:24 +0100

    wow eindelijk

--- homework/week_6/linked_views.js
+++ homework/week_6/linked_views.js
@@ -2,37 +2,22 @@ window.onload = function() {
 
   var format = d3.format(",");
 
-    footer = d3.select("footer")
-
-    footer.append("p")
-          .append("text")
-          .text("Heroine and cocaine retail- and wholeprices in Europe and US")
-          .attr("class", "head")
-          .append("p")
-          .append("text")
-          .text("Name: Kimberley Boersma, student number: 11003464")
-          .attr("class", "h2")
-          .append("p")
-          .text("Data source:")
-          .append("a")
-          .attr("xlink:href", "https://dataunodc.un.org/drugs/heroin_and_cocaine_prices_in_eu_and_usa"+"Data Source");
-
-
 // Set tooltips
 var tip = d3.tip()
             .attr('class', 'd3-tip')
-            .offset([-10, 0])
+            .offset([0, 0])
             .html(function(d) {
-              return "<strong>Country: </strong><span class='details'>" + d.properties.name + "<br></span>" + "<strong>Population: </strong><span class='details'>" + format(d.population) +"</span>";
+              return "<strong>Country: </strong><span class='details'>" + d.properties.name + "<br></span>" + "<strong>Cocaine retail price: </strong><span class='details'>" + d.retailprices +"US$ per gram</span>";
             })
 
-var margin = {top: -100, right: 0, bottom: 0, left: 0},
-            width = 960 - margin.left - margin.right,
-            height = 500 - margin.top - margin.bottom;
+var margin = {top: -130, right: 0, bottom: 0, left: 0},
+            width = 1000 - margin.left - margin.right,
+            height = 400 - margin.top - margin.bottom,
+            min = 50,
+            max = 120;
 
-var color = d3.scaleThreshold()
-    .domain([10000,100000,500000,1000000,5000000,10000000,50000000,100000000,500000000,1500000000])
-    .range(["rgb(247,251,255)", "rgb(222,235,247)", "rgb(198,219,239)", "rgb(158,202,225)", "rgb(107,174,214)", "rgb(66,146,198)","rgb(33,113,181)","rgb(8,81,156)","rgb(8,48,107)","rgb(3,19,43)"]);
+var color = d3.scaleSequential(d3.interpolateOrRd)
+              .domain([min, max]);
 
 var path = d3.geoPath();
 
@@ -44,8 +29,8 @@ var svg = d3.select("body")
             .attr('class', 'map');
 
 var projection = d3.geoMercator()
-                   .scale(130)
-                  .translate( [width / 2, height / 1.5]);
+                   .scale(210)
+                  .translate( [width / 1.2, height / 0.95]);
 
 var path = d3.geoPath().projection(projection);
 
@@ -75,15 +60,43 @@ Promise.all(requests).then(function(response) {
     hero_whole_eu = response[5]
     coca_whole_us = response[6]
     hero_whole_us = response[7]
-    console.log(coca_ret_eu)
     data = response[8]
-    population = response[9]
+    retail = response[9]
     svg = response[10]
 
-    var populationById = {};
+    all = arrayValues(coca_ret_eu)
+    cweuArray = arrayValues(coca_whole_eu)
+    creu = all[0]
+    creu_all = all[1]
+    maxCreu = Math.max.apply(null, creu_all)
+    years = all[2]
+    minYear = Math.min.apply(null, years)
+    maxYear = Math.max.apply(null, years)
+    creu_dict = all[3]
+    countriesEU = Object.keys(coca_ret_eu);
+    console.log(countriesEU)
+    countryUS = "Average inflation adjusted in 2016 US$"
 
-      population.forEach(function(d) { populationById[d.id] = +d.population; });
-      data.features.forEach(function(d) { d.population = populationById[d.id] });
+    creuArray = xyArrays(coca_ret_eu, countriesEU)
+    cweu = xyArrays(coca_whole_eu, countriesEU)
+    hreu = xyArrays(hero_ret_eu, countriesEU)
+    hweu = xyArrays(hero_whole_eu, countriesEU)
+    crus = xyArrays(coca_ret_us, countryUS)
+    cwus = xyArrays(coca_whole_us, countryUS)
+    hrus = xyArrays(hero_ret_us, countryUS)
+    hwus = xyArrays(hero_whole_us, countryUS)
+
+    dictRetailEU = makeDictionary(creuArray, hreu, countriesEU)
+    dictRetailUS = makeDictionary(crus, hrus, "USA")
+    dictWholeEU = makeDictionary(cweu, hweu, countriesEU)
+    dictWholeUS = makeDictionary(cwus, hwus, "USA")
+
+    update(dictRetailEU, countriesEU, years, minYear, maxYear, "Austria")
+
+    var retailById = {};
+
+      retail.forEach(function(d) { retailById[d.id] = +d.retailprices; });
+      data.features.forEach(function(d) { d.retailprices = retailById[d.id] });
 
       svg.append("g")
           .attr("class", "countries")
@@ -91,7 +104,7 @@ Promise.all(requests).then(function(response) {
           .data(data.features)
         .enter().append("path")
           .attr("d", path)
-          .style("fill", function(d) { return color(populationById[d.id]); })
+          .style("fill", function(d) { return color(retailById[d.id]); })
           .style('stroke', 'white')
           .style('stroke-width', 1.5)
           .style("opacity",0.8)
@@ -113,25 +126,114 @@ Promise.all(requests).then(function(response) {
                 .style("opacity", 0.8)
                 .style("stroke","white")
                 .style("stroke-width",0.3);
+            })
+            .on("mousedown", function(d) {
+              if (d.properties.name == "USA") {
+                update(dictRetailUS, countriesEU, years, minYear, maxYear, d.properties.name)
+              } else if (countriesEU.includes(d.properties.name)) {
+                update(dictRetailEU, countriesEU, years, minYear, maxYear, d.properties.name)
+              } else {
+                d3.select("#graph").remove();
+              }
             });
 
       svg.append("path")
           .datum(topojson.mesh(data.features, function(a, b) { return a.id !== b.id; }))
-           // .datum(topojson.mesh(data.features, function(a, b) { return a !== b; }))
           .attr("class", "names")
           .attr("d", path);
 
-    all = arrayValues(coca_ret_eu)
-    creu = all[0]
-    creu_all = all[1]
-    minY = Math.min.apply(null, creu_all)
-    maxY = Math.max.apply(null, creu_all)
-    years = all[2]
-    minX = Math.min.apply(null, years)
-    maxX = Math.max.apply(null, years)
-    creu_dict = all[3]
-    countries = Object.keys(coca_ret_eu);
 
+}).catch(function(e){
+    throw(e);
+});
+
+};
+
+function arrayValues(data) {
+  values = []
+  values_all = []
+  dicto = []
+  for (key in data) {
+    temp = []
+    years = []
+    temp_values = []
+    countries = data[key]
+    for (key2 in countries){
+        temp.push({
+          country: key,
+          year: Number(key2),
+          value: countries[key2]
+        })
+      years.push(Number(key2))
+      temp_values.push(countries[key2])
+      values_all.push(countries[key2])
+    }
+    dicto.push(temp)
+    values.push(temp_values)
+  }
+  return [values, values_all, years, dicto]
+
+  }
+
+function calcMinMax(data, data2, country) {
+  allValues = []
+    dataList = [data[country], data2[country]]
+      dataList.forEach(function(d){
+        for (value in d) {
+          allValues.push(d[value]) }
+        });
+  minValues = Math.min.apply(null, allValues)
+  maxValues = Math.max.apply(null, allValues)
+  return [minValues, maxValues];
+}
+
+function xyArrays(data, countries) {
+    dict = {}
+    try {
+      countries.forEach(function(key){
+          temp = []
+          for (year in data[key]) {
+            tempxy = [Number(year), data[key][year]]
+            temp.push(tempxy)
+          }
+          dict[key] = temp
+      });
+    } catch(err) {
+      temp = []
+      for (year in data[countries]) {
+          tempxy = [Number(year), data[countries][year]]
+          temp.push(tempxy)
+      }
+      dict["USA"] = temp
+    };
+    return dict;
+}
+
+function makeDictionary(data, data2, countries) {
+    dict = {}
+    try {
+      countries.forEach(function(key){
+        dict[key] = [data[key], data2[key]]
+      });
+    } catch(err) {
+      dict["USA"] = [data[countries], data2[countries]]
+    };
+    return dict;
+}
+
+  function update(data, countries, years, minX, maxX, id){
+    // remove whole svg
+    d3.select("#graph").remove();
+
+    if (countries.includes(id)) {
+      minMax = calcMinMax(coca_ret_eu, hero_ret_eu, id)
+      minY = minMax[0]
+      maxY = minMax[1]
+    } else {
+        minMaxUS = calcMinMax(coca_ret_us, hero_ret_us, countryUS)
+        minY = minMaxUS[0]
+        maxY = minMaxUS[1]
+    }
     // create margins and padding
     var margin = {top: 10, right: 20, bottom: 20, left: 10},
       padding = {top: 20, right: 60, bottom: 60, left: 30},
@@ -142,9 +244,9 @@ Promise.all(requests).then(function(response) {
       width = innerWidth - padding.left - padding.right,
       height = innerHeight - padding.top - padding.bottom;
 
-      const colorScale = d3.schemeCategory10 // d3.scaleOrdinal(d3[categorical[0].name]);
+      var title = 'Cocaine and heroin retailprices (streetprices) in ' + id
 
-      const title = 'Cocaine retailprices (streetprices) in Western Europe';
+      lineID = ["Cocaine retailprices", "Heroin retailprices"]
 
       // create x and y scale
       var xScale = d3.scaleLinear()
@@ -157,6 +259,8 @@ Promise.all(requests).then(function(response) {
         .domain([0, maxY])
         .nice();
 
+        console.log(maxY)
+
       // x and y axis to scale
       var xAxis = d3.axisBottom(xScale)
                   .tickFormat(function(d){ return d })
@@ -168,6 +272,7 @@ Promise.all(requests).then(function(response) {
 
       // create svg
       var svg = d3.select("body").append("svg")
+          .attr("id", "graph")
           .attr("width", outerWidth)
           .attr("height", outerHeight)
           .append("g")
@@ -213,23 +318,26 @@ Promise.all(requests).then(function(response) {
 
       var dataNest = d3.nest()
               .key(function(d, i) {return d[0].country;})
-              .entries(creu_dict);
+              .entries(data);
 
       // define the line
       var valueline = d3.line()
-          .x(function(d) { return xScale(d.year) })
-          .y(function(d) { return yScale(d.value) })
+          .x(function(d) { return xScale(d[0]) })
+          .y(function(d) { return yScale(d[1]) })
           .curve(d3.curveMonotoneX); // apply smoothing to the line
 
-      var returnValue = (function(d) { return d.value});
+      console.log(data[id])
 
       // Append the path, bind the data, and call the line generator
-      for (i = 0; i < creu_dict.length; i++){
+      data[id].forEach(function(d, i){
         g.append("path")
             .attr("class", "line") // Assign a class for styling
-            .attr("d", valueline(creu_dict[i])) // Calls the line generator
-            .attr("id", countries[i])
-            .style("stroke", function(d, t ) { return colorScale[i % 10]; })
+            .attr("d", valueline(d)) // Calls the line generator
+            .attr("id", lineID[i])
+            .style("stroke", function(d) {
+              if (i == 0) { return "#6c069b"}
+              else { return "#964d04" };
+              })
             .on("mouseover", function(){
                 d3.select(this)
                 .attr("opacity", 0.4)
@@ -254,39 +362,8 @@ Promise.all(requests).then(function(response) {
             tooltip.append("text")
                     .attr("x", 15)
                     .attr("dy", "1.2em")
-                    .style("font-size", "0.9em")
-                    .attr("font-weight", "light");
-      }
-
-
-}).catch(function(e){
-    throw(e);
-});
-
-};
-
-function arrayValues(data) {
-  values = []
-  values_all = []
-  dicto = []
-  for (key in data) {
-    temp = []
-    years = []
-    temp_values = []
-    countries = data[key]
-    for (key2 in countries){
-        temp.push({
-          country: key,
-          year: Number(key2),
-          value: countries[key2]
-        })
-      years.push(Number(key2))
-      temp_values.push(countries[key2])
-      values_all.push(countries[key2])
-    }
-    dicto.push(temp)
-    values.push(temp_values)
-  }
-  return [values, values_all, years, dicto]
-
+                    .style("font-size", "1.1em")
+                    .attr("font-weight", "bold")
+                    .attr("fill", "#0043ff");
+      });
   }
